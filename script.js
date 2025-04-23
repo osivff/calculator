ed5786272d150475607f737026ab85e6b455580b
osivff <oscar.fitchfierro@gmail.com>
2025-04-23 06:05:01 -0700

    Display numbers

--- script.js
+++ script.js
@@ -1,39 +1,50 @@
 const input = document.querySelector('.input-field');
 const buttons =  document.querySelector('table');
 let number = [];
+let result = 0;
+
+function addNum(arr){
+    let result = arr.reduce((sumTotal, current) => {
+        return sumTotal += current;
+    }, 0)
+    return result;
+}
+
+function subtractNum(arr){
+    let result = arr.reduce((total, current) => {
+        return total -= current;
+    }, 0)
+    return result;
+}
+
+function multiplyNum(arr){
+    let result = arr.reduce((product, current) => {
+        return product *= current;
+    }, 0)
+    return result;
+}
+
+function divideNum(arr){
+    let result = arr.reduce((quotient, current) => {
+        return quotient /= current;
+    }, 0)
+    return result;
+}
+
 
 buttons.addEventListener('click', (e) => {
     let attribute = e.target.getAttribute('class');
+
     if(e.target.tagName === 'BUTTON' && attribute === 'number'){
-        input.value += Number(e.target.innerText);
-    }
+        input.value += e.target.innerText;
+    } 
 });
 
-
-function addNum(...args){
-    let result = args.reduce((sumTotal, current) => {
-        return sumTotal += current;
-    })
-    return result;
-}
-
-function subtractNum(...args){
-    let result = args.reduce((total, current) => {
-        return total -= current;
-    })
-    return result;
-}
-
-function multiplyNum(...args){
-    let result = args.reduce((product, current) => {
-        return product *= current;
-    })
-    return result;
-}
-
-function divideNum(...args){
-    let result = args.reduce((quotient, current) => {
-        return quotient /= current;
-    })
-    return result;
-}
+buttons.addEventListener('click', (e) => {
+    let attribute = e.target.getAttribute('class');
+    
+    if(e.target.tagName === 'BUTTON' && attribute === 'orange'){
+        number.push(Number(input.value));
+        input.value = '';
+    }
+});
