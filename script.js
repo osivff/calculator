f8d934ed391fbb633be720717a2793c81f9bc868
osivff <oscar.fitchfierro@gmail.com>
2025-04-26 18:42:18 -0700

    Display result

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const input = document.querySelector('.input-field');
 const buttons =  document.querySelector('table');
+const display = document.querySelector('.display');
+const span = document.createElement('span');
+
 let number = [];
 let result = 0;
+
 
 function addNum(arr){
     let result = arr.reduce((sumTotal, current) => {
@@ -27,7 +31,7 @@ function multiplyNum(arr){
 function divideNum(arr){
     let result = arr.reduce((quotient, current) => {
         return quotient /= current;
-    }, 0)
+    })
     return result;
 }
 
@@ -46,22 +50,42 @@ buttons.addEventListener('click', (e) => {
     let innerText = e.target.innerText;
     let lastChar = input.value[input.value.length - 1];
 
-
     if(tagName === 'BUTTON' && innerText === '+' && input.value != 0){
         if(!isNaN(lastChar)){
+            let removeLast = [];
+            let mappedNum;
+
             input.value += innerText;
+            number = input.value.split('+');
+            removeLast = number.pop();
+            mappedNum = number.map((num) => {return Number(num)});
+            console.log(addNum(mappedNum));
+            span.textContent = `${addNum(mappedNum)}`;
+            display.appendChild(span);
         }
     } else if (innerText === '÷' && input.value != 0){
         if(!isNaN(lastChar)){
+            let removeLast = [];
+            let mappedNum;
+
             input.value += innerText;
+            number = input.value.split('÷')
+
+            removeLast = number.pop();
+            mappedNum = number.map((num) => {return Number(num)});
+            console.log(divideNum(mappedNum));
+            span.textContent = `${divideNum(mappedNum)}`;
+            display.appendChild(span);
         }
     } else if (innerText === '×' && input.value != 0){
         if(!isNaN(lastChar)){
             input.value += innerText;
+            number = input.value.split('×');
         }
     } else if (innerText === '−' && input.value != 0){
         if(!isNaN(lastChar)){
             input.value += innerText;
+            number = input.value.split('−');
         }
     } else if (innerText === '.' && input.value != 0){
         if(!isNaN(lastChar)){
@@ -69,3 +93,4 @@ buttons.addEventListener('click', (e) => {
         }
     }
 })
+
