ca11f76d1f99307e53afcd3ef6304ef7a04c41b8
osivff <oscar.fitchfierro@gmail.com>
2025-04-27 18:22:12 -0700

    Add logic to use multiple numbers and operators

--- script.js
+++ script.js
@@ -1,95 +1,117 @@
 const input = document.querySelector('.input-field');
 const buttons =  document.querySelector('table');
 const display = document.querySelector('.display');
-const span = document.createElement('span');
+let span = document.createElement('span');
 
-let number = [];
-let result = 0;
+let currentValue = '';
+let operator = '';
+let savedValue = null;
 
-
-function addNum(arr){
-    let result = arr.reduce((sumTotal, current) => {
-        return sumTotal += current;
-    }, 0)
-    return result;
+function operate(op, num, current){
+    if(op === '+') return num + current;
+    if(op === '−') return num - current;
+    if(op === '×') return num * current;
+    if(op === '÷') return num / current;
 }
 
-function subtractNum(arr){
-    let result = arr.reduce((total, current) => {
-        return total -= current;
-    }, 0)
-    return result;
-}
-
-function multiplyNum(arr){
-    let result = arr.reduce((product, current) => {
-        return product *= current;
-    }, 0)
-    return result;
-}
-
-function divideNum(arr){
-    let result = arr.reduce((quotient, current) => {
-        return quotient /= current;
-    })
-    return result;
-}
-
-
-buttons.addEventListener('click', (e) => {
+let handleButtonClick = buttons.addEventListener('click', (e) => {
     let attribute = e.target.getAttribute('class');
+    let innerText = e.target.innerText; 
 
-    if(e.target.tagName === 'BUTTON' && attribute === 'number'){
+    if(e.target.tagName === 'BUTTON' && (attribute === 'number' || innerText === '.')){
         input.value += e.target.innerText;
+        currentValue += e.target.innerText;
     }
 });
 
-buttons.addEventListener('click', (e) => {
-    let attribute = e.target.getAttribute('class');
-    let tagName = e.target.tagName;
+let handleOperatorClick = buttons.addEventListener('click', (e) => {
     let innerText = e.target.innerText;
     let lastChar = input.value[input.value.length - 1];
 
-    if(tagName === 'BUTTON' && innerText === '+' && input.value != 0){
+    if(innerText === '+' && input.value != 0){
         if(!isNaN(lastChar)){
-            let removeLast = [];
-            let mappedNum;
-
             input.value += innerText;
-            number = input.value.split('+');
-            removeLast = number.pop();
-            mappedNum = number.map((num) => {return Number(num)});
-            console.log(addNum(mappedNum));
-            span.textContent = `${addNum(mappedNum)}`;
-            display.appendChild(span);
+            operator = innerText;
+
+            if(savedValue === null){
+                savedValue = Number(currentValue);
+            } else {
+                savedValue += Number(currentValue);
+            }
+            currentValue = '';
         }
     } else if (innerText === '÷' && input.value != 0){
         if(!isNaN(lastChar)){
-            let removeLast = [];
-            let mappedNum;
-
             input.value += innerText;
-            number = input.value.split('÷')
+            operator = innerText;
 
-            removeLast = number.pop();
-            mappedNum = number.map((num) => {return Number(num)});
-            console.log(divideNum(mappedNum));
-            span.textContent = `${divideNum(mappedNum)}`;
-            display.appendChild(span);
+            if(savedValue === null){
+                savedValue = Number(currentValue);
+                currentValue = '';
+            } else if(currentValue !== '') {
+                savedValue = Number(savedValue) / Number(currentValue);
+                currentValue = '';
+            }
         }
     } else if (innerText === '×' && input.value != 0){
         if(!isNaN(lastChar)){
             input.value += innerText;
-            number = input.value.split('×');
+            operator = innerText;
+
+            if(savedValue === null){
+                savedValue = Number(currentValue);
+                currentValue = '';
+            } else if(savedValue !== ''){
+                savedValue = Number(savedValue) * Number(currentValue);
+                currentValue = '';
+            }
         }
     } else if (innerText === '−' && input.value != 0){
         if(!isNaN(lastChar)){
             input.value += innerText;
-            number = input.value.split('−');
+            operator = innerText;
+
+            if(savedValue === null){
+                savedValue = Number(currentValue);
+            } else {
+                savedValue = Number(savedValue) - Number(currentValue);
+            }
+            currentValue = '';
         }
-    } else if (innerText === '.' && input.value != 0){
-        if(!isNaN(lastChar)){
-            input.value += innerText;
+    } else if(innerText === 'AC'){
+        input.value = '';
+        savedValue = null;
+        operator = '';
+        currentValue = '';
+    }
+});
+
+let handleEqualClick = buttons.addEventListener('click', (e) => {
+    let innerText = e.target.innerText;
+    let numOne = Number(savedValue);
+    let numTwo = Number(currentValue);
+
+    if(innerText === '=' && input.value != 0){
+        if(operator === '+'){
+            input.value = `${operate(operator ,numOne, numTwo)}`;
+            savedValue = operate(operator ,numOne, numTwo);
+            currentValue = '';
+            operator = '';
+        } else if(operator === '−'){
+            input.value = `${operate(operator ,numOne, numTwo)}`;
+            savedValue = operate(operator ,numOne, numTwo);
+            currentValue = '';
+            operator = '';
+        } else if(operator === '×'){
+            input.value = `${operate(operator ,numOne, numTwo)}`;
+            savedValue = operate(operator ,numOne, numTwo);
+            currentValue = '';
+            operator = '';
+        } else if(operator === '÷'){
+            input.value = `${operate(operator ,numOne, numTwo)}`;
+            savedValue = operate(operator ,numOne, numTwo);
+            currentValue = '';
+            operator = '';
         }
     }
 })
